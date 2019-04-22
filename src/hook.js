6105fe2a5095fe46697066eacee848da9d9291b3
Shelly Leung <shelly@joinreel.com>
2019-04-21 19:31:29 -0700

    hook function fix - typo

--- src/hook.js
+++ src/hook.js
@@ -3,17 +3,10 @@ import React, { useState } from 'react';
 export default function Counter() {
 	const [count, setCount] = useState(0);
 
-	function handleClickAlert() {
-		setTimeout(() => {
-			console.log('You clicked on: ' + count);
-		}, 3000);
-	}
-
 	return (
 		<div>
 			<p> Button clicked {count} times.</p>
-			<button onCLick={() => setCount(count + 1)}>Click</button>
-			<button onClick={handleClickAlert}>Alert</button>
+			<button onClick={() => setCount(count + 1)}>Click</button>
 		</div>
 	);
 }
